a5a69245dbcb9b5bcc039714de7c7ecaa924e0f6
冷斌 <xykxyk2008@163.com>
2019-12-26 11:01:09 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# jwt
-jwt  token 黑名单
+Info
+-------
+jwt 刷新token 黑名单
+
+Request
+-------
+```
+"lcobucci/jwt": "^3.3"
+```
+
+Usage
+-----
+```php
+
+
+<?php
+    // 方法一
+    $config = [
+        //加密类型, 支持类型请看 supportedAlgs
+        'alg'        => 256,
+        //私钥，可以是字符串也可以是文件路径
+        'privateKey' => '',
+        //公钥，可以是字符串也可以是文件路径
+        'publicKey'  => '',
+        //key,
+        'key'        => 'ice',
+        //发行人
+        'iss'        => '',
+        //接受者
+        'aud'        => '',
+        //在多少秒之前不可使用
+        'nbf'        => 0,
+        //过期时间
+        'exp'        => 7200,
+        // 黑名单缓存类,
+        'cache'      => \Cache::class, // implement Psr\SimpleCache\CacheInterface
+    ];
+    $jwt = new Jwt($config);
+    // make
+    $jwt->makeToken(["a" => '1', "b" => '2'], 1);
+    $exp = 604800;
+    $refreshToken = $jwt->makeRefreshToken($exp);
+    
+    // validate
+    $token = '';
+    $jwt->verify($token);
+     // get params
+    $jwt->claimsAsArray();
+
+     // refreshToken
+    $jwt->refreshToken($refreshToken);
+
+    $jwt->logout();
+
+    // 方法二  依赖注入
+    //自己去实现
+    // OauthInterface => TokenFactory
+    // TokenFactory 实现方法一
+```
+
+
+
---
+++ src/Blacklist.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lengbin\Jwt;
+
+use Lengbin\Hyperf\Helper\ObjectHelper;
+use Psr\SimpleCache\CacheInterface;
+
+class Blacklist extends ObjectHelper
+{
+    /**
+     * @var CacheInterface
+     */
+    public $cache;
+
+    public $prefix = 'JWT-';
+
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+        if (!$this->cache instanceof CacheInterface) {
+            throw new \InvalidArgumentException(get_class($this->cache) . ' must implement ' . CacheInterface::class);
+        }
+    }
+
+    /**
+     * token 加入缓存
+     *
+     * @param array $claims        [
+     *                             jti, // 唯一id, uid
+     *                             iat, //发布时间
+     *                             exp, //到期时间
+     *                             ]
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function add(array $claims)
+    {
+        $exp = $claims['exp'];
+        $iat = $claims['iat'];
+        $id = $claims['jti'];
+        $claims['blacklist_register_at'] = time();
+        $this->cache->set($this->prefix . $id, $claims, ($exp - $iat));
+    }
+
+    /**
+     * 是否在黑名单中
+     *
+     * @param string $name
+     * @param array  $claims
+     *
+     * @return bool
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function has(string $name, array $claims)
+    {
+        $isOverdue = false;
+        $data = $this->get($name);
+        if (!empty($data) && !empty($claims)) {
+            $isOverdue = ($data['blacklist_register_at'] - $claims['iat']) > 1;
+        }
+        return $isOverdue;
+    }
+
+    /**
+     * 加入 黑名单
+     *
+     * @param $name
+     *
+     * @return bool
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function join($name)
+    {
+        $claims = $this->get($name);
+        if (empty($claims)) {
+            return false;
+        }
+        $exp = $claims['exp'];
+        $timeRemaining = $exp - time();
+        if ($timeRemaining <= 0) {
+            $this->delete($name);
+            return false;
+        }
+        $claims['blacklist_register_at'] = time();
+        $this->cache->set($this->prefix . $name, $claims, $timeRemaining);
+        return true;
+    }
+
+    /**
+     * 获得 缓存
+     *
+     * @param $name
+     *
+     * @return mixed
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function get($name)
+    {
+        return $this->cache->get($this->prefix . $name);
+    }
+
+    /**
+     * 移除 缓存
+     *
+     * @param $name
+     *
+     * @return mixed
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function delete($name)
+    {
+        return $this->cache->delete($this->prefix . $name);
+    }
+
+}
